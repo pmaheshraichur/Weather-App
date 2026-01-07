ed6c9c7f4b5d34184136aa5815010d61abfd538b
pmaheshraichur <pmaheshraichur@gmail.com>
2026-01-07 12:35:03 +0530

    Fix: allow Render host in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // allows access from network
+  },
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: ['weather-app-axgt.onrender.com'],
+  }
+});
